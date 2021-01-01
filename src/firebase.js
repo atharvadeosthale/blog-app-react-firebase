c8acf4929c861576ea5709ba82844b309d063c80
Atharva Deosthale <atharva@Atharvas-MacBook-Pro.local>
2021-01-01 23:53:36 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjPEFs16fO5bwZn5fvGaqfkxAm25A_LCQ",
+  authDomain: "blog-app-firebase-51f15.firebaseapp.com",
+  projectId: "blog-app-firebase-51f15",
+  storageBucket: "blog-app-firebase-51f15.appspot.com",
+  messagingSenderId: "1060530942430",
+  appId: "1:1060530942430:web:a8b2acd24a141d2317ad4f",
+  measurementId: "G-YQBJGBDGHK",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+export const auth = app.auth();
+export const db = app.firestore();
