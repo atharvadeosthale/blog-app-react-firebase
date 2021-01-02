9c3c5fbe0d76c755a6217379774d14f041e4570e
Atharva Deosthale <atharva@Atharvas-MacBook-Pro.local>
2021-01-03 01:13:48 +0530

    Made blog card

---
+++ src/BlogCard.js
@@ -0,0 +1,39 @@
+import {
+  CommentOutlined,
+  ShareOutlined,
+  ThumbDownOutlined,
+  ThumbUpOutlined,
+} from "@material-ui/icons";
+import React from "react";
+import "./BlogCard.css";
+
+function BlogCard({ title, image, description, slug }) {
+  return (
+    <div className="blogCard">
+      <div className="blogCard__mainContainer">
+        <div className="blogCard__image">
+          <img src={image} className="blogCard__img" />
+        </div>
+        <div className="blogCard__contentBox">
+          <div className="blogCard__contentBoxDetails">
+            <h1>{title}</h1>
+            <p className="blogCard__description">{description}</p>
+          </div>
+          <div className="blogCard__contentBoxOptions">
+            <div className="blogCard__options">
+              <ThumbUpOutlined className="blogCard__option" />
+              0
+              <ThumbDownOutlined className="blogCard__option" />0
+              <CommentOutlined className="blogCard__option" />0
+            </div>
+            <div className="blogCard__options">
+              <ShareOutlined className="blogCard__option" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BlogCard;
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BlogCard from "./BlogCard";
 import "./Home.css";
 import Intro from "./Intro";
 
@@ -8,6 +9,14 @@ function Home() {
       <Intro />
       <div className="home__blogsHeader">
         <span className="home__blogsHeaderText">BLOGS</span>
+      </div>
+      <div className="container home__blogs">
+        <BlogCard
+          description="In this blog I tell you why having a portfolio website is important."
+          title="Importance of having a portfolio"
+          slug="importance-of-having-portfolio"
+          image="https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2020/07/shutterstock_582803470.jpg?w=750"
+        />
       </div>
     </div>
   );
